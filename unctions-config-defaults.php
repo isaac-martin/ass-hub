480f45e9771d86a3f264a4562ec80633f8576961
isaac-martin <isaac@weareplayground.co>
2017-03-24 17:15:15 -0400

    build and push

--- unctions-config-defaults.php
+++
@@ -1,6 +0,0 @@
-<?php // ==== CONFIGURATION (DEFAULT) ==== //
-
-// Specify default configuration values here; these may be overwritten by previously defined values in `functions-config.php`
-
-// Switch for WP AJAX Page Loader; true/false
-defined( 'asshub_SCRIPTS_PAGELOAD' )       || define( 'asshub_SCRIPTS_PAGELOAD', true );
